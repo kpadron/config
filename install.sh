8241d584ed8bfaecf89ef0dc2d51db1248fc991e
Kristian Padron <kpadron.github@gmail.com>
2018-08-24 03:41:37 -0400

    Disable the dreaded bell!

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
-file_list="profile bashrc bash_aliases bash_functions nanorc gitconfig"
+file_list="profile bashrc bash_aliases bash_functions inputrc nanorc gitconfig"
 
 for file in $file_list; do
 	if ! diff -N -q "$file" "${HOME}/.$file"; then
